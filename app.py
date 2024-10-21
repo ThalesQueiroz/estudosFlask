84bb976f0c751fe0688d8e47701ce8a4e0e04334
ThalesQueiroz <148828539+ThalesQueiroz@users.noreply.github.com>
2024-10-21 10:16:43 -0300

    aula 1.2
    Aprendendo a utilizar o request e o reder_template

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Olá Mundo!"
+    return "<h1>Olá Mundo!<h1>"
+
+@app.route("/instagram")
+def aluno ():
+    return render_template('formulario.html')
+
+@app.route("/envio", nethod=['POST'])
+def envioForms():
+    nombre=request.form['username']
+    passowrd
